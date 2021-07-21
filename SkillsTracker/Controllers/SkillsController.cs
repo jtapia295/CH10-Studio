8efe44e2fd0ea5803cf2a8309a657a4661f35f0d
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-07-21 11:24:35 -0400

    Started CH10 Studio

---
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsTracker.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpGet]
+        [Route("/skills")]
+        public IActionResult Skills(string date, )
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route ("/skills/form")]
+        public IActionResult Form()
+        {
+            string html = @"
+<form method='post' action='/skills'>
+<label name ='Date'><input type='date'/></label>
+<label for='C'>C#:</label>
+<select id = 'C'>
+<option value = '
+</select>
+                                            " ;
+        }
+    }
+}
