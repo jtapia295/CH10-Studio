3c059f0ac4fb2018fe5bfb5d99e940d1626c0303
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-07-21 17:03:54 -0400

    Added Material for CH10 Studio

--- SkillsTracker/Controllers/SkillsController.cs
+++ SkillsTracker/Controllers/SkillsController.cs
@@ -8,32 +8,64 @@ namespace SkillsTracker.Controllers
 {
     public class SkillsController : Controller
     {
+
+        List<string> skillsToLearn = new List<string> {"C#","JavaScript","Python" };
         public IActionResult Index()
         {
             return View();
         }
 
         
-        [HttpGet]
-        [Route("/skills")]
-        public IActionResult Skills(string date, )
+        [HttpGet("/skills")]
+        public IActionResult Skills()
         {
+            string html = $@"
+            <h1> Skills Tracker </h1>
+            <h2> Coding Skills to Learn: </h2 >
+            <ol >
+                <li>{skillsToLearn[0]}</li>
+                <li>{skillsToLearn[1]}</li>
+                <li>{skillsToLearn[2]}</li>
+            </ol >";
 
+            return Content(html,"text/html");
+        }
+
+        [HttpPost("/skills/form")]
+        public IActionResult Form(string date, string )
+        {
             return View();
         }
 
-        [HttpPost]
-        [Route ("/skills/form")]
-        public IActionResult Form()
+
+        [HttpGet("/skills/form")]
+        public IActionResult Formbuilder()
         {
-            string html = @"
-<form method='post' action='/skills'>
-<label name ='Date'><input type='date'/></label>
-<label for='C'>C#:</label>
-<select id = 'C'>
-<option value = '
-</select>
-                                            " ;
+            string html = $@"
+            <form method='post' action='/skills'>
+            <label name ='Date'><input type='date'/></label>
+
+            <label for='C'>{skillsToLearn[0]}</label>
+            <select name='{skillsToLearn[0]}' id='C'>
+            <option value = 'Making Apps'> Making Apps </option>
+            <option value = 'Master Coder'> Master Coder </option>
+            <option value = 'Learning Basics'>Learning Basics</option>
+
+            <label for='Java'>{skillsToLearn[1]}</label>
+            <select name='{skillsToLearn[1]}' id='Java'>
+            <option value = 'Making Apps'> Making Apps </option>
+            <option value = 'Master Coder'> Master Coder </option>
+            <option value = 'Learning Basics'>Learning Basics</option>
+            </select >
+
+            <label for='C'>{skillsToLearn[2]}</label>
+            <select name='{skillsToLearn[2]}' id='C'>
+            <option value = 'Making Apps'> Making Apps </option>
+            <option value = 'Master Coder'> Master Coder </option>
+            <option value = 'Learning Basics'>Learning Basics</option>
+                                                " ;
+
+            return View();
         }
     }
 }
